6449f5ab571911c84c15577137451c29127f2f78
Celia Chao <celia.chao@ufv.es>
2022-10-26 20:33:13 +0200

    Prueba 1 hecha

---
+++ src/main/java/com/ufv/dis/PruebaSimple.java
@@ -0,0 +1,12 @@
+package com.ufv.dis;
+
+public class PruebaSimple {
+    public boolean prueba1(int a, int b){
+        if (a>b){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
